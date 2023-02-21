e8aade9ebdf52dffbe5646d20f1d68d7e0c098c1
saysetel <saysetel@gmail.com>
2023-02-21 14:02:22 +0100

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Anastasia Kotova on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
